4f004cd39aefdbd65672feb186dee2b140e35fa0
Kamil <kamilbieg96@gmail.com>
2019-11-16 13:49:06 +0100

    GameThread extends Thread now

--- peter-pan/src/main/java/GameThread.java
+++ peter-pan/src/main/java/GameThread.java
@@ -5,27 +5,31 @@ import players.PlayersManagement;
 
 import java.util.Arrays;
 
-public class GameThread {
+public class GameThread extends Thread {
 
     private static final int NUMBER_OF_PLAYERS = 3;
     private int[] winners;
-    private int winnersCounter = 0;
+    private int winnersCounter;
 
-    public GameThread() {
-        PlayersCollection playersCollection = new PlayersCollection();
+    private PlayersManagement playersManagement;
+
+    GameThread() {
         Cards cards = new Cards();
         cards.shuffle();
 
         winners = new int[NUMBER_OF_PLAYERS];
-
-        playersCollection.createPlayers(NUMBER_OF_PLAYERS);
-
-        PlayersManagement playersManagement = new PlayersManagement(playersCollection);
-        playersManagement.dealTheCards(cards.getCardsCollection());
-        playersManagement.showPlayersHands();
-
         Arrays.fill(winners, -1);
 
+        PlayersCollection playersCollection = new PlayersCollection();
+        playersCollection.createPlayers(NUMBER_OF_PLAYERS);
+
+        playersManagement = new PlayersManagement(playersCollection);
+        playersManagement.dealTheCards(cards.getCardsCollection());
+    }
+
+    @Override
+    public void run() {
+        winnersCounter = 0;
         while (winnersCounter < NUMBER_OF_PLAYERS - 1) {
             for (int i = 0; i < NUMBER_OF_PLAYERS; i++) {
                 if (winners[i] != 1) {
@@ -44,11 +48,14 @@ public class GameThread {
             }
         }
         for (int i = 0; i < winners.length; i++)
-            System.out.println("Player " + (i + 1) + ": " + winners[i]);
+            if (winners[i] == 1)
+                System.out.println("Player " + (i + 1) + ": Winner");
+            else
+                System.out.println("Player " + (i + 1) + ": Looser");
     }
-    private void setWinner(int id){
+
+    private void setWinner(int id) {
         winners[id] = 1;
-        System.out.println("Win!!!!!!!!!!!!!!!!!!!!!!!!!" + id);
         winnersCounter++;
     }
 
@@ -59,6 +66,5 @@ public class GameThread {
             return nextPlayerId(id + 1);
         else
             return id + 1 < NUMBER_OF_PLAYERS ? id + 1 : 0;
-
     }
 }
--- peter-pan/src/main/java/MainApp.java
+++ peter-pan/src/main/java/MainApp.java
@@ -1,5 +1,6 @@
 public class MainApp {
     public static void main(String[] args) {
         GameThread gameThread = new GameThread();
+        gameThread.start();
     }
 }
